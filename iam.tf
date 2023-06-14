063dcbbb0cb5d93e04115069920775036444d04c
MykytaNovitskyi <mykyta.novitskyi.it.2022@lpnu.ua>
2023-06-14 17:18:54 +0300

    added Domain 2

--- iam.tf
+++ iam.tf
@@ -71,3 +71,51 @@ resource "aws_iam_role" "instance" {
   path               = "/system/"
   assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
 }
+
+resource "aws_lambda_permission" "allow_api_gateway_authors" {
+  statement_id  = "AllowExecutionFromAPIGateWay"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda.lambda_authors_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/GET${aws_api_gateway_resource.authors.path}"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway_courses" {
+  statement_id  = "AllowExecutionFromAPIGateWay"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda.lambda_courses_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/GET${aws_api_gateway_resource.courses.path}"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway_courses_save" {
+  statement_id  = "AllowExecutionFromAPIGateWay"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda.lambda_courses_save_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/POST${aws_api_gateway_resource.courses.path}"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway_courses_update" {
+  statement_id  = "AllowExecutionFromAPIGateWay"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda.lambda_courses_update_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/PUT${aws_api_gateway_resource.courses.path}/*"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway_courses_get" {
+  statement_id  = "AllowExecutionFromAPIGateWay"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda.lambda_courses_get_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/GET${aws_api_gateway_resource.courses.path}/*"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway_courses_delete" {
+  statement_id  = "AllowExecutionFromAPIGateWay"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda.lambda_courses_delete_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/DELETE${aws_api_gateway_resource.courses.path}/*"
+}
--- labels.tf
+++ labels.tf
@@ -6,3 +6,11 @@ module "label" {
   environment = var.environment
   delimiter   = var.delimiter
 }
+
+module "label_api" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
+  name    = "api"
+  context = module.label.context
+}
