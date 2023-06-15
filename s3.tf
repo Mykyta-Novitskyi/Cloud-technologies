c6ed1209b757e3dad247bee4a26d04a6e831e18c
MykytaNovitskyi <mykyta.novitskyi.it.2022@lpnu.ua>
2023-06-15 18:27:43 +0300

    added Domain 3

--- s3.tf
+++ s3.tf
@@ -18,3 +18,36 @@ resource "aws_s3_bucket" "this2" {
     Environment = local.env
   }
 }
+
+module "s3_bucket_frontend" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.9.0"
+
+  bucket = format("%s-s3", module.label.id)
+
+  versioning = {
+    enabled = false
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = module.s3_bucket_frontend.s3_bucket_id
+  policy = data.aws_iam_policy_document.this.json
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["${module.cdn.cloudfront_origin_access_identity_iam_arns[0]}"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${module.s3_bucket_frontend.s3_bucket_arn}/*",
+    ]
+  }
+}
